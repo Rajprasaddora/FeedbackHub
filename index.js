a98dc38cdb7a8644c7b150930609f9e0d0fc21ed
rajprasad <rajprasaddora2000@gmail.com>
2021-07-27 18:08:49 +0530

    added default route for server

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 //node js don't have ES15 module access so import will not work
 const app = express();
 
-app.get('/',(req,res)=>{
-    
-})
+app.get("/", (req, res) => {
+    res.send({ hi: "there" });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
